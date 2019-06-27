fb23ecf0e05827a9d40e6a1f7c6d3796402acb4e
sehui <sehui@17weiju.com>
2019-06-27 17:43:22 +0800

    vue-webpack-config:production

---
+++ build/webpack-config.js
@@ -0,0 +1,36 @@
+const Path = require('path');
+// const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+let config = {
+    entry: '../src/main.js',
+    output: {
+        filename: 'vendor.build.js',
+        path: Path.jion(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            loader: 'vue-loader'
+        },
+        {
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        },
+        {
+            test: /\.(png|jpg|jpeg|gif)$/,
+            use: {
+                loader: 'url-loader',
+                options: {
+                    limit: 1024, // 判断图片的大小   如果小于1024就会转换成base64
+                    name: '[name].[ext]' // 输出图片的名字  ext是扩展名
+                }
+            }],
+    }
+    // plugins: [
+    //     new VueLoaderPlugin()
+    // ]
+}
+module.exports = config
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app"></div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data () {
+        return{}
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
